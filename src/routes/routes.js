3b1af6022e0a62c4fd899f0b477e2374c667daf4
Douglas Cavalcante <38563983+douglas-cavalcante@users.noreply.github.com>
2023-07-08 20:24:11 -0300

    adiciona api

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import DragonForm from '../pages/DragonForm';
 import DragonList from '../pages/DragonList';
